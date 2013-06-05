99ecb7e2292fb421dedf765a3b4f70b2fb0cf39d
Michal Špaček <tupinek@gmail.com>
2013-06-06 00:33:46 +0200

    Remove (3pm) from doc.

--- NoDie.pm
+++ NoDie.pm
@@ -172,19 +172,19 @@ Error::Pure::NoDie - Error::Pure module for simple print instead die.
 
 =head1 DEPENDENCIES
 
-L<Exporter(3pm)>,
-L<List::MoreUtils(3pm)>,
-L<Readonly(3pm)>.
+L<Exporter>,
+L<List::MoreUtils>,
+L<Readonly>.
 
 =head1 SEE ALSO
 
-L<Error::Pure(3pm)>,
-L<Error::Pure::AllError(3pm)>,
-L<Error::Pure::Die(3pm)>,
-L<Error::Pure::Error(3pm)>,
-L<Error::Pure::ErrorList(3pm)>,
-L<Error::Pure::Output::Text(3pm)>,
-L<Error::Pure::Print(3pm)>.
+L<Error::Pure>,
+L<Error::Pure::AllError>,
+L<Error::Pure::Die>,
+L<Error::Pure::Error>,
+L<Error::Pure::ErrorList>,
+L<Error::Pure::Output::Text>,
+L<Error::Pure::Print>.
 
 =head1 AUTHOR
 
