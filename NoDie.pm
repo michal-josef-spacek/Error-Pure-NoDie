db2ca3adcb5565fa0927c8afe5b8cf328cf1af6f
Michal Špaček <tupinek@gmail.com>
2013-06-11 15:48:56 +0200

    Prepare for 0.02 version.

--- NoDie.pm
+++ NoDie.pm
@@ -11,7 +11,7 @@ use List::MoreUtils qw(none);
 use Readonly;
 
 # Version.
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constants.
 Readonly::Array our @EXPORT_OK => qw(err);
@@ -197,6 +197,6 @@ BSD license.
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
